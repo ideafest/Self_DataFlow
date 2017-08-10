e107ec7bcb52c00a3d3efa74a570213b1f85eb19
Akshay <akshays@zimetrics.com>
2017-08-10 14:44:06 +0530

    Great success!

--- src/main/java/com/Practice/Basic/BasicTest2.java
+++ src/main/java/com/Practice/Basic/BasicTest2.java
@@ -43,7 +43,7 @@ public class BasicTest2 {
 		}
 	}
 	
-	static PCollection<String> combineTableDetails(PCollection<TableRow> stringPCollection1, PCollection<TableRow> stringPCollection2){
+	static PCollection<TableRow> combineTableDetails(PCollection<TableRow> stringPCollection1, PCollection<TableRow> stringPCollection2){
 		
 		PCollection<KV<String, TableRow>> kvpCollection1 = stringPCollection1.apply(ParDo.named("FormatData1").of(new BasicTest1.ReadFromTable1()));
 		PCollection<KV<String, TableRow>> kvpCollection2 = stringPCollection2.apply(ParDo.named("FormatData2").of(new BasicTest1.ReadFromTable2()));
@@ -57,8 +57,8 @@ public class BasicTest2 {
 				.apply(CoGroupByKey.create());
 		
 		
-		PCollection<String> resultPCollection = pCollection
-				.apply(ParDo.named("Result").of(new DoFn<KV<String, CoGbkResult>, String>() {
+		PCollection<TableRow> resultPCollection = pCollection
+				.apply(ParDo.named("Result").of(new DoFn<KV<String, CoGbkResult>, TableRow>() {
 					@Override
 					public void processElement(ProcessContext context) throws Exception {
 						KV<String, CoGbkResult> element = context.element();
@@ -84,9 +84,8 @@ public class BasicTest2 {
 							for(FieldMetaData metaData : fieldMetaDataList2){
 								newRow.set(metaData.getName(), row2Details.get(metaData.getName()));
 							}
+							context.output(newRow);
 						}
-						context.output(newRow.toString());
-						
 					}
 				}));
 		
@@ -102,18 +101,17 @@ public class BasicTest2 {
 	
 	public static void main(String[] args) {
 		
-//		List<TableFieldSchema> fieldSchemaList = new ArrayList<>();
-//		fieldSchemaList.add(new TableFieldSchema().setName("campaignid").setType("STRING"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("createddate").setType("TIMESTAMP"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("updateddate").setType("TIMESTAMP"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("prospecthandleduration").setType("INTEGER"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("DNI").setType("STRING"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("campaignid").setType("STRING"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("call_date").setType("DATE"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("record_datetime").setType("TIMESTAMP"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("status").setType("STRING"));
-//		fieldSchemaList.add(new TableFieldSchema().setName("dispositionstatus").setType("STRING"));
-//		TableSchema tableSchema = new TableSchema().setFields(fieldSchemaList);
+		List<TableFieldSchema> fieldSchemaList = new ArrayList<>();
+		fieldSchemaList.add(new TableFieldSchema().setName("campaignid").setType("STRING"));
+		fieldSchemaList.add(new TableFieldSchema().setName("createddate").setType("TIMESTAMP"));
+		fieldSchemaList.add(new TableFieldSchema().setName("updateddate").setType("TIMESTAMP"));
+		fieldSchemaList.add(new TableFieldSchema().setName("prospecthandleduration").setType("INTEGER"));
+		fieldSchemaList.add(new TableFieldSchema().setName("DNI").setType("STRING"));
+		fieldSchemaList.add(new TableFieldSchema().setName("call_date").setType("DATE"));
+		fieldSchemaList.add(new TableFieldSchema().setName("record_datetime").setType("TIMESTAMP"));
+		fieldSchemaList.add(new TableFieldSchema().setName("status").setType("STRING"));
+		fieldSchemaList.add(new TableFieldSchema().setName("dispositionstatus").setType("STRING"));
+		TableSchema tableSchema = new TableSchema().setFields(fieldSchemaList);
 		
 		BasicTest1.Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(BasicTest1.Options.class);
 		Pipeline pipeline = Pipeline.create(options);
@@ -122,9 +120,12 @@ public class BasicTest2 {
 		PCollection<TableRow> rowPCollection1 = pipeline.apply(BigQueryIO.Read.named("Reader1").from(table1Name));
 		PCollection<TableRow> rowPCollection2 = pipeline.apply(BigQueryIO.Read.named("Reader2").from(table2Name));
 		
-		PCollection<String> pCollection = combineTableDetails(rowPCollection1, rowPCollection2);
+		PCollection<TableRow> pCollection = combineTableDetails(rowPCollection1, rowPCollection2);
 		
-		pCollection.apply(TextIO.Write.named("Writer").to(options.getOutput()));
+		pCollection.apply(BigQueryIO.Write.named("Writer").to(options.getOutput())
+		.withSchema(tableSchema)
+		.withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+		.withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE));
 		
 		pipeline.run();
 		
@@ -161,7 +162,7 @@ public class BasicTest2 {
 	
 	private static List<FieldMetaData> getTableSchemaDetails(String datasetName, String tableName){
 		List<FieldMetaData> fieldMetaDataList = new ArrayList<>();
-		for(Field field: getThemFields("Learning","Table1")){
+		for(Field field: getThemFields(datasetName,tableName)){
 			fieldMetaDataList.add(new FieldMetaData(field.getName(), field.getType().getValue().toString()));
 		}
 		return fieldMetaDataList;
